2178e0383809905b6672f8e2499919c60bc3e265
EriikGabriel <erikgabriel.lins@hotmail.com>
2026-07-30 14:57:49 -0300

    feat: custom exception responses

---
+++ src/main/java/br/com/erik/spring_boot_essentials/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package br.com.erik.spring_boot_essentials.exception;
+
+public class NotFoundException extends Exception {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+
+}
